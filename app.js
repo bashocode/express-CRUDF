22ec499f4dac2629224f9109b2a9c4547d9dd002
dani <ikhdamuhammad@gmail.com>
2020-01-28 10:10:52 +0700

    add finish

--- app.js
+++ app.js
@@ -18,4 +18,16 @@ app.get('/', (req, res) => {
 	res.render('index', { data });
 });
 
+app.get('/add', (req, res) => {
+	res.render('add');
+})
+
+app.post('/add', (req, res) => {
+	const { title, country } = req.body;
+
+	data.push({ ID: data.length + 1, Title: title, Country: country });
+	fs.writeFileSync('./data/series.json', JSON.stringify(data, null, 4));
+	res.redirect('/');
+})
+
 app.listen(port, () => console.log(`json-bread listening on port ${port}!`));
